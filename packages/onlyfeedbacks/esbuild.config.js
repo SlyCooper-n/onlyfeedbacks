e6b4538d7f916d9d3ca89ecfcdbd6464046364c7
Gabriel <gabrielvitor.frasao@gmail.com>
2022-11-25 20:25:27 -0300

    build: add esbuild config file

---
+++ packages/onlyfeedbacks/esbuild.config.js
@@ -0,0 +1,10 @@
+// const res = require("esbuild").buildSync({
+//   entryPoints: ["src/App.tsx"],
+//   outfile: "dist/index.js",
+//   minify: true,
+//   sourcemap: true,
+//   loader: {
+//     ".svg": "file",
+//   },
+//   jsx: "preserve",
+// });
