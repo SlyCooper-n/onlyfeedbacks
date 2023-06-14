2b948c0cb1f9c64e9469e51ff24fe71d9d895c80
Gabriel <gabrielvitor.frasao@gmail.com>
2023-06-13 23:12:00 -0300

    style(react): format classes

--- packages/react/src/components/modules/Loading.tsx
+++ packages/react/src/components/modules/Loading.tsx
@@ -2,8 +2,8 @@ import { CircleNotch } from "phosphor-react";
 
 export const Loading = () => {
   return (
-    <div className="w-6 h-6 flex justify-center items-center">
-      <CircleNotch weight="bold" className="w-4 h-4 animate-spin" />
+    <div className="flex h-6 w-6 items-center justify-center">
+      <CircleNotch weight="bold" className="h-4 w-4 animate-spin" />
     </div>
   );
 };
