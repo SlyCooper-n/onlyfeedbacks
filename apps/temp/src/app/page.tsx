20c5d33cdc8420e1a4b2649d9b109509b494c8b8
Gabriel <gabrielvitor.frasao@gmail.com>
2023-06-13 23:14:26 -0300

    refactor: match package changes

--- apps/temp/src/app/page.tsx
+++ apps/temp/src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <div>
       <Widget.Root
-        feedbacks={[{
+        feedbacks={[
+          {
             type: "aitoma",
             title: "Aitoma",
             image: {
@@ -13,46 +14,54 @@ export default function Home() {
               alt: "Next.js"
             },
             inputPlaceholder: "What's your name?",
-        }, {
-          type: "xablau",
-          title: "Xablau",
-          image: {
-            src: "/vercel.svg",
-            alt: "Vercel"
           },
-          inputPlaceholder: "What's your name?",
-      }, {
-        type: "esqueça",
-        title: "Esqueça",
-        image: {
-          src: "/next.svg",
-          alt: "Next.js"
-        },
-        inputPlaceholder: "What's your name?",
-    }, {
-      type: "xablau dnv",
-      title: "Xablau dnv",
-      image: {
-        src: "/vercel.svg",
-        alt: "Vercel"
-      },
-      inputPlaceholder: "What's your name?",
-  }, {
-    type: "ai-caliquinha",
-    title: "Ai caliquinha",
-    image: {
-      src: "/next.svg",
-      alt: "Next.js"
-    },
-    inputPlaceholder: "What's your name?",
-},]}
+          {
+              type: "xablau",
+              title: "Xablau",
+              image: {
+                src: "/vercel.svg",
+                alt: "Vercel"
+              },
+              inputPlaceholder: "What's your name?",
+          },
+          {
+            type: "esqueça",
+            title: "Esqueça",
+            image: {
+              src: "/next.svg",
+              alt: "Next.js"
+            },
+            inputPlaceholder: "What's your name?",
+          },
+          {
+            type: "xablau dnv",
+            title: "Xablau dnv",
+            image: {
+              src: "/vercel.svg",
+              alt: "Vercel"
+            },
+            inputPlaceholder: "What's your name?",
+          },
+          {
+            type: "ai-caliquinha",
+            title: "Ai caliquinha",
+            image: {
+              src: "/next.svg",
+              alt: "Next.js"
+            },
+            inputPlaceholder: "What's your name?",
+          },
+        ]}
         serverEndpoint="http://localhost:3333/feedbacks"
+        actions={[
+          {
+            flow: "aham",
+            label: "Aham",
+            component: <div>AHAAAAAMM</div>
+          }
+        ]}
       >
         <Widget.Content>
-          <Widget.DisableButton />
-
-          <Widget.Divider />
-
           <Widget.Link href="https://github.com/gabe-frasz" target="_blank">aitoma</Widget.Link>
         </Widget.Content>
 
